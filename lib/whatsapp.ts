2362d14ba86c783a9659ffd0bb680731e0d982db
adr!an <111903096+adrianvrj@users.noreply.github.com>
2025-08-31 10:10:31 -0600

    #19 change wha number

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -48,7 +48,7 @@ export async function sendMembershipReminderWhatsApp(
 
     const messageData = {
       contentSid: "HXa8721e8e848c4c0da3a721625b4c628c", // Your Twilio template SID
-      from: "whatsapp:+17409486674", // Twilio Sandbox WhatsApp number (replace with your number in production)
+      from: "whatsapp:+19519441781", // Twilio Sandbox WhatsApp number (replace with your number in production)
       to: `whatsapp:${formattedPhone}`,
       contentVariables: JSON.stringify({
         "1": gymName,
